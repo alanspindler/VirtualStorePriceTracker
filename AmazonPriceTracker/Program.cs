644dd695c1a0a9c9c03c04d742adbe042158156f
Alan Spindler <alan.rafael@it4solution.com.br>
2024-08-21 13:36:11 -0400

    Parallel execution first test

--- AmazonPriceTracker/Program.cs
+++ AmazonPriceTracker/Program.cs
@@ -9,11 +9,10 @@
             {
                 IdLogExecution = InsertExecutionLogStart();
                 var fillTask = FillEmptyProductNames();
-                var updatePriceTask = UpdateProductPrice();
+                var updatePriceTask = UpdateProductPrices();
                 var notifyUsersTask = CheckAndNotifyUsersAsync();
                 await Task.WhenAll(fillTask, updatePriceTask, notifyUsersTask);
 
-                // Atualiza o registro de execução como sucesso
                 UpdateExecutionLogSuccess(IdLogExecution);
             }
 
@@ -21,25 +20,6 @@
             {
                 UpdateExecutionLogError(IdLogExecution, ex.ToString());
             }
-
-            //await page.RouteAsync("**/*", (route) =>
-            //{
-            //    var url = route.Request.Url;
-            //    if (url.Contains("https://aax-us-east-retail-direct.amazon.com/e/xsp/getAd?placementId") || url.Contains("https://unagi.amazon.com.br/1/events/com.amazon.csm.csa.prod") || url.Contains("https://completion.amazon.com.br/api/2017/suggestions") || url.Contains("https://unagi-na.amazon.com/1/events/com.amazon.eel.SponsoredProductsEventTracking.prod"))
-            //    {
-            //        return route.AbortAsync();
-            //    }
-            //    return route.ContinueAsync();
-            //});
-
-            //await page.RouteAsync("https://www.amazon.com.br/dram/renderLazyLoaded", (route) =>
-            //{
-            //    return route.AbortAsync();
-            //});
-            //await page.RouteAsync("https://aax-us-east-retail-direct.amazon.com/e/xsp/getAd?placementId", (route) =>
-            //{
-            //    return route.AbortAsync();
-            //});
 
             await Task.Delay(TimeSpan.FromMinutes(10));
         }
