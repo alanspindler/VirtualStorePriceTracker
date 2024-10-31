c09c8769b2726e39ab9754fa186a6271743fd5f1
Alan Spindler <alanspindler@live.com>
2024-10-31 09:27:53 -0400

    Enabled headless mode

--- VirtualStorePriceTracker/Functions.cs
+++ VirtualStorePriceTracker/Functions.cs
@@ -676,7 +676,7 @@ namespace Functions
         public static async Task UpdateProductPrices()
         {
             var playwright = await Playwright.CreateAsync();
-            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
             var context = await browser.NewContextAsync(new BrowserNewContextOptions
             {
                 UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.4692.99 Safari/537.36"
